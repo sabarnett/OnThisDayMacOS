807e17e9b12dc782339c31fc6c3b6007f9e2d7dd
Steven barnett <steve@sabarnett.co.uk>
2022-09-22 15:43:44 +0100

    Initial Commit

---
+++ OnThisDay/OnThisDayApp.swift
@@ -0,0 +1,15 @@
+// Project: OnThisDay
+//
+//  
+//
+
+import SwiftUI
+
+@main
+struct OnThisDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
